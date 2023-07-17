c8a5dd1cc05addfa1c28d6b7f7cea506afc4dc32
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-17 21:23:29 +0300

    api connect and send data to tutorials componenet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,40 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
-
+import { useEffect,useState } from "react";
+import axios  from "axios";
 const Home = () => {
+
+  const [tutorial, setTutorial] = useState([])
+  const BASE_URL="https://tutorial-api.fullstack.clarusway.com/tutorials/"
+  
+  const getTutorials=async()=>{
+
+    try {
+
+      const {data} = await axios(BASE_URL)
+      setTutorial(data)
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+    
+  }
+
+  console.log(tutorial);
+ 
+  useEffect(() => {
+    
+    getTutorials()
+
+  }, [])
+  
+
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials={tutorial} />
     </>
   );
 };
